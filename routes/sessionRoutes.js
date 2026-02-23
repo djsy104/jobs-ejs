af7f4498ced080974de506e14c6da992d5038fd1
djsy104 <148027370+djsy104@users.noreply.github.com>
2026-02-22 16:28:53 -0800

    Week14: Partial route implementation

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -21,5 +21,16 @@ router
     }),
   );
 router.route("/logoff").post(logoff);
+router.route("/jobs").get().post();
+router.route("/jobs/new").get();
+router.route("/jobs/edit/:id").get();
+router.route("/jobs/update/:id").post();
+router.route("/jobs/delete/:id").post();
+
+// POST /jobs (Add a new job listing)
+// GET /jobs/new (Put up the form to create a new entry)
+// GET /jobs/edit/:id (Get a particular entry and show it in the edit box)
+// POST /jobs/update/:id (Update a particular entry)
+// POST /jobs/delete/:id (Delete an entry)
 
 module.exports = router;
